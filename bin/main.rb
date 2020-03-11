3c69542f00a94a2e2af5568d192c04be3e2f4508
kyle-law <junwei_1121@hotmail.com>
2020-03-11 21:49:17 +0800

    Optimize Player class

--- bin/main.rb
+++ bin/main.rb
@@ -2,8 +2,17 @@
 require_relative '../lib/game.rb'
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
+title = <<-MLS
 
-puts 'Welcome to Tic Tac Toe'
+ _____ _        _____             _____           
+/__   (_) ___  /__   \\__ _  ___  /__   \\___   ___ 
+  / /\\/ |/ __|   / /\\/ _` |/ __|   / /\\/ _ \\ / _ \\
+ / /  | | (__   / / | (_| | (__   / / | (_) |  __/
+ \\/   |_|\\___|  \\/   \\__,_|\\___|  \\/   \\___/ \\___|
+                                                  
+MLS
+puts title
+puts 'Welcome to Tic Tac Toe!'
 
 puts 'Please enter Player 1 name'
 player1 = gets.chomp
@@ -12,7 +21,8 @@ player2 = gets.chomp
 
 game = Game.new
 board = Board.new
-player = Player.new
+playerX = Player.new
+playerO = Player.new
 
 puts <<~MLS
       Rule: #{player1} gets the first chance to select a cell from the board.
@@ -28,19 +38,19 @@ loop do
     puts 'Error! Please enter a cell number that is not filled.'
     player1_move = gets.chomp.to_i
   end
+  game.delete_available_cells(player1_move)
 
-  player.player_move('X', player1_move)
+  playerX.move(player1_move)
 
   board.board_config('X', player1_move)
   puts board.display_board
-
-  if game.win?(player.player1_cells)
+  if game.win?(playerX.cells)
     puts "Hurray! #{player1} has won"
   elsif game.draw?
     puts 'It\'s a draw.'
   end
 
-  break if game.win?(player.player1_cells) || game.draw?
+  break if game.win?(playerX.cells) || game.draw?
 
   # Input Player O
   puts "#{player2}, select a cell from the above board"
@@ -50,17 +60,18 @@ loop do
     puts 'Error! Please enter a cell number that is not filled.'
     player2_move = gets.chomp.to_i
   end
+  game.delete_available_cells(player2_move)
 
-  player.player_move('O', player2_move)
+  playerO.move(player2_move)
 
   board.board_config('O', player2_move)
   puts board.display_board
 
-  if game.win?(player.player2_cells)
+  if game.win?(playerO.cells)
     puts "Hurray! #{player2} has won"
   elsif game.draw?
     puts 'It\'s a draw.'
   end
 
-  break if game.win?(player.player2_cells) || game.draw?
+  break if game.win?(playerO.cells) || game.draw?
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,18 @@
 class Player
-  attr_accessor :player1_cells, :player2_cells
+  attr_accessor :cells
   
   def initialize
-    @player1_cells = []
-    @player2_cells = []
+    # @player1_cells = []
+    # @player2_cells = []
+    @cells = []
   end
 
-  def player_move(player, move)
-    @player1_cells << move if player == 'X'
-    @player2_cells << move if player == 'O'
+  def move(move)
+    @cells << move
+    # @player1_cells << move if player == 'X'
+    # @player2_cells << move if player == 'O'
   end
 end
+
+# kyle = Player.new => my_array(my_cells) = [], kyle.player_move(5) , my_cells << 5
+# tirtha = Player.new => [] , tirtha.move
